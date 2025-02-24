8fb1f8eb5db822d18d6ba80e83dc185618a5ea8f
Luiz Fernando <fernandinho.manon@gmail.com>
2025-02-24 18:36:40 -0300

    feat: Implementa um programa que imprime a tabuada do 5 de 1 até 10

---
+++ exercicio-02-tabuada/tabuada.dart
@@ -0,0 +1,13 @@
+// Exercício 02 - Tabuada
+
+// PROBLEMA: Crie um programa que imprima a tabuada do 5 (de 1 a 10)
+
+// SOLUÇÃO
+
+void main() {
+  // Loop de 0 a 10
+  for(int i = 1; i <= 10; i++) {
+    // Imprime o resultado de i multiplicado por 5
+    print(i * 5);
+  }  
+}
