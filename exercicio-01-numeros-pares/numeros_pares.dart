aa82d2a3314960417f81de6c28b254987ae8b03c
Luiz Fernando <fernandinho.manon@gmail.com>
2025-02-24 18:35:43 -0300

    feat: Implementa um programa que imprime números pares de 0 a 20

---
+++ exercicio-01-numeros-pares/numeros_pares.dart
@@ -0,0 +1,13 @@
+// Exercício 01 - Numeros Pares
+
+// PROBlEMA: Crie um programa que imprima todos os números pares de 0 a 20.
+
+// SOLUÇÃO
+
+void main() {
+  // Inicia o loop de 0 até 20, pulando de 2 em 2
+  for(int i = 0; i <= 20; i+= 2) {
+    // Imprime o número par atual
+    print(i);
+  }
+}
